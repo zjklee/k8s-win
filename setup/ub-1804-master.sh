1bb353f592037c72191ab5642354518430f53b84
Oleh Formaniuk <zjklee@gmail.com>
2021-06-01 18:30:41 -0700

    Update ub-1804-master.sh

--- setup/ub-1804-master.sh
+++ setup/ub-1804-master.sh
@@ -5,7 +5,7 @@ SERVICE_CIDR="10.96.0.0/12"
 
 # initialise Kube
 
-kubeadm init --pod-network-cidr=$POD_NETWORK_CIDR --service-cidr=$SERVICE_CIDR --apiserver-advertise-address 192.168.42.2 --apiserver-cert-extra-sans control-plane --control-plane-endpoint control-plane
+kubeadm init --pod-network-cidr=$POD_NETWORK_CIDR --service-cidr=$SERVICE_CIDR --apiserver-advertise-address 192.168.42.2 --apiserver-cert-extra-sans 192.168.42.2 --control-plane-endpoint 192.168.42.2
 
 # install Flannel pod network
 wget https://raw.githubusercontent.com/zjklee/k8s-win/master/setup/flannel/kube-flannel-hybrid.yaml
